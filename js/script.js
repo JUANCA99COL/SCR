083dcf89b2588a6c170bf57190d1f1def70b62bd
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2024-08-20 16:23:52 +1200

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// toggle menu js 
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+  
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
+
+// slideshows 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// aos animations 
+AOS.init({
+  delay: 200, // values from 0 to 3000, with step 50ms
+  duration: 1500, // values from 0 to 3000, with step 50ms
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+});
